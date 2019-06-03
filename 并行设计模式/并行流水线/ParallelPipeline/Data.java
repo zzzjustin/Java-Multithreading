9770db31d12c34fff58add8c1b526e17c08562ac
justinzeng <zengjustin@126.com>
2019-06-03 16:03:23 +0800

    Create Data.java
    2019/6/3 Update.

---
+++ 并行设计模式/并行流水线/ParallelPipeline/Data.java
@@ -0,0 +1,17 @@
+package com.justin.parallelpipeline;
+
+public class Data {
+	public double A, B, C, D;
+	public String resule;
+	
+	public Data() {
+		
+	}
+	
+	public Data(double A, Double B, Double C, Double D) {
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.D = D;
+	}
+}
